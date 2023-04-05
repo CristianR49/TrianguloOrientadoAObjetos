efaf7b9381d74d470a5d6657936fac9d458504fb
Cristian <cristian.rr99@gmail.com>
2023-04-05 20:46:44 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Drawing;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace TrianguloOrientadoAObjetos
+{
+    internal class Program
+    {
+        public int x, y, z;
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Triangulo triangulo = new Triangulo();
+                do
+                {
+                    Console.Clear();
+                    Console.WriteLine("Digite o valor de X");
+                    triangulo.x = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Digite o valor de Y");
+                    triangulo.y = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Digite o valor de Z");
+                    triangulo.z = Convert.ToInt32(Console.ReadLine());
+
+                    if (triangulo.InvalidacaoDosLados())
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("valores inválidos (O comprimento de qual quer um dos lados do triângulo precisa ser menor do que a soma dos outros dois)");
+                        Console.ReadLine();
+                        Console.ResetColor();
+                    }
+                } while (triangulo.InvalidacaoDosLados());
+
+                if (triangulo.x == triangulo.y && triangulo.y == triangulo.z)
+                {
+                    Console.WriteLine("É um triangulo equilátero");
+                }
+                else if (triangulo.x != triangulo.y && triangulo.y != triangulo.z)
+                {
+                    Console.WriteLine("É um triangulo escaleno");
+                }
+                else if (triangulo.ValidacaoTrianguloIsoceles())
+                {
+                    Console.WriteLine("É um triangulo isóceles");
+                }
+                Console.ReadLine();
+            }
+
+
+
+        }
+    }
+}
---
+++ Triangulo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrianguloOrientadoAObjetos
+{
+    public class Triangulo
+    {
+        public int x, y, z;
+
+        public bool InvalidacaoDosLados()
+        {
+            return x + y < z || x + z < y || z + y < x;
+        }
+        public bool ValidacaoTrianguloIsoceles()
+        {
+            return x !=  y && z == x || z != x && y == z || y != z && y == x;
+        }
+    }
+}
